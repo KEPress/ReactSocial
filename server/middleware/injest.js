ee08655f62262dd883920a8afd45377a8cc03ed0
KEPress <kepressltd@gmail.com>
2026-05-17 21:13:34 -0400

    9th commit - injest script mod

--- server/middleware/injest.js
+++ server/middleware/injest.js
@@ -3,47 +3,47 @@ const { UserModel } = require('../models/user.model')
 
 const inngest = new Inngest({ id: 'reactsocial' })
 
-// Sync User Creation
+//Sync User Creation
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
+    { id: 'sync-user-from-clerk', triggers: [{ event: 'clerk/user.created' }] },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         let username = email_addresses[0].email_address.split('@')[0]
 
-        const existingUser = await UserModel.findOne({ username })
-        if (existingUser) username = username + Math.floor(Math.random() * 10000)
-
-        await UserModel.create({
-            _id: id,
-            email: email_addresses[0].email_address,
-            full_name: first_name + ' ' + last_name,
+        //Check availability of username
+        const user = await UserModel.findOne({ username: username })
+        if (user) username = username + Math.floor(Math.random() * 10000)
+        const userData = ({
+            _id: id, email: email_addresses[0].email_address,
+            full_name: (first_name + (" ") + last_name),
             profile_picture: image_url,
-            username
+            username 
         })
-    }
+        
+        await UserModel.create(userData)
+    } //end function
 )
 
-// Sync User Update
+//Sync User Update
 const syncUserUpdate = inngest.createFunction(
-    { id: 'update-user-from-clerk' },
-    { event: 'clerk/user.updated' },
+    { id: 'update-user-from-clerk', triggers: [{ event: 'clerk/user.updated' }] },
     async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url, username } = event.data
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
 
-        await UserModel.findByIdAndUpdate(id, {
+        const updateUserData = ({
             email: email_addresses[0].email_address,
-            full_name: first_name + ' ' + last_name,
+            full_name: (first_name + (" ") + last_name),
             profile_picture: image_url,
             username
         })
+
+        await UserModel.findByIdAndUpdate(id, updateUserData)
     }
 )
 
-// Sync User Deletion
+//Sync User Deletion
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user.deleted' },
+    { id: 'delete-user-with-clerk', triggers: [{ event: 'clerk/user.deleted' }] },
     async ({ event }) => {
         const { id } = event.data
         await UserModel.findByIdAndDelete(id)
@@ -51,5 +51,6 @@ const syncUserDeletion = inngest.createFunction(
 )
 
 const functions = [syncUserCreation, syncUserUpdate, syncUserDeletion]
+
 
 module.exports = { inngest, functions }
