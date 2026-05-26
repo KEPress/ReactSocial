950c5d903fb7472dc37ebcac25cd8ff3d4ab161b
KEPress <kepressltd@gmail.com>
2026-05-25 20:53:10 -0400

    20th commit

--- server/config/authorize.js
+++ server/config/authorize.js
@@ -3,7 +3,7 @@ const { getAuth } = require('@clerk/express')
 exports.protect = async (request, response, next) => {
     try {
         const { userId } = await getAuth(request)
-        if (!userId) return response.json({ success: false, message: ('Unauthenticated') })
+        if (!userId) return response.json({ success: false, data: userId, message: ('Unauthenticated') })
         next()
     } catch (error) {
         response.json({ success: false, message: (error.message) })
