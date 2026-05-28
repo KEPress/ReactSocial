193c849f63c9ca7b534a637d31ea1a0c89c98599
KEPress <kepressltd@gmail.com>
2026-05-28 02:35:42 -0400

    22nd commit inngest fix

--- server/middleware/injest.js
+++ server/middleware/injest.js
@@ -18,10 +18,9 @@ const syncUserCreation = inngest.createFunction(
         const userData = await UserModel.create({
             _id: id, email: email_addresses[0].email_address,
             full_name: (first_name + (" ") + last_name),
-            profile_picture: image_url,
-            username 
+            profile_picture: image_url
         })
-        
+
         return(userData)
     } //end function
 )
