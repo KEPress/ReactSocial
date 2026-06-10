e7cc3729f5c7c6b29cc716c5ff4e1e79aa189b25
KEPress <kepressltd@gmail.com>
2026-06-09 23:10:13 -0400

    37th commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { setServers } = require('dns')
 const { serve } = require('inngest/express')
-const { clerkMiddleware, getAuth } = require('@clerk/express')
+const { clerkMiddleware } = require('@clerk/express')
 const { database_connect } = require('./config/database')
 const { userRoute } = require('./router/user.route')
 const { postRoute } = require('./router/post.route')
@@ -18,7 +18,7 @@ application.use(clerkMiddleware())
 
 application.get(`/`, (request, response) => response.send('Server is online'))
 application.use(`/api/inngest`, serve({ client: inngest, functions }))
-application.use(`/api/post`, postRoute)
+application.use(`/api/user`, userRoute).use(`/api/post`, postRoute)
 
 const PORT = (process.env.PORT || (4000))
 
